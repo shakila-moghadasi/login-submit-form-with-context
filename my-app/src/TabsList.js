8ea531a0f1d3940874380791b6f214a003073bbe
shakila moghadasi <moghadasishakila239@gmail.com>
2022-04-22 21:32:54 +0430

    -

--- my-app/src/TabsList.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const TabsList = (props) => {
-    const { children } = props;
-    return <ul className="tabs__list" >{children}</ul>;
-  };
-  
-export default TabsList;
